056aed1e5a3c0a5a26e46b6754122a18b9257a2d
MattPM <mattmule@gmail.com>
2020-01-22 16:21:55 -0500

    Added vignette

--- R/cells_citeseq_mtx.r
+++ R/cells_citeseq_mtx.r
@@ -1,9 +1,9 @@
 #' @title sample CITEseq protein data 87 protein by 2872 cells
 #'
-#' @description A \code{matrix} of cells by antibodies Raw CITEseq data used for example scripts of the dsb package. raw data processed with CITE-seq-count https://hoohm.github.io/CITE-seq-Count/
+#' @description A matrix of cells by antibodies Raw CITEseq data used for example scripts of the dsb package. raw data processed with CITE-seq-count https://hoohm.github.io/CITE-seq-Count/
 #'
-#' @format A \code{matrix} 87 protein by 2872 cells
+#' @format A matrix 87 protein by 2872 cells
 #' \describe {
-#' cells_citeseq_mtx is a matrix of cells as columns and 87 proteins as rows. It is a random even distribution of cells manually annotated from protein clustering of maximum of 100 cells per cluster ( on 30 clusters). Full celltype data annotations and more information is available in the referenced publication.
+#' cells_citeseq_mtx is a matrix of cells as columns and 87 proteins as rows. It is a random even distribution of cells manually annotated from protein clustering of maximum of 100 cells per cluster on 30 clusters. Full celltype data annotations and more information is available in the referenced publication.
 #' }
 "cells_citeseq_mtx"
