87463f8bac305e2c5b5303d3675c27732ec2cbbc
MattPM <mattmule@gmail.com>
2020-02-23 19:46:45 -0500

    fix data description

--- R/cells_citeseq_mtx.r
+++ R/cells_citeseq_mtx.r
@@ -4,6 +4,6 @@
 #'
 #' @format A matrix 87 protein by 2872 cells
 #' \describe {
-#' cells_citeseq_mtx is a matrix of cells as columns and 87 proteins as rows. It is a random even distribution of cells manually annotated from protein clustering of maximum of 100 cells per cluster on 30 clusters. Full celltype data annotations and more information is available in the referenced publication.
+#' \item{cells_citeseq_mtx}{is an R matrix of cells as columns and 87 proteins as rows. It is a random even distribution of cells manually annotated from protein clustering of maximum of 100 cells per cluster on 30 clusters. Full celltype data annotations and more information is available in the referenced publication.}
 #' }
 "cells_citeseq_mtx"
--- R/empty_drop_citeseq_mtx.R
+++ R/empty_drop_citeseq_mtx.R
@@ -4,6 +4,6 @@
 #'
 #' @format A matrix 87 protein by 8005 empty droplets confirmed by cell hashing and cross referencing sample genotypes.
 #' \describe {
-#' empty_drop_citeseq_mtx is a matrix of empty droplets from a CITEseq experiment using cell hashing antibodies called by Seurat's HTODemux function as negative cells, called by Demuxlet via referencing patient genotype data as ambiguous drops, and with less than 80 unique genes detected. This is used for robust estimation of the background distribution of each protein.
+#' \item{empty_drop_citeseq_mtx}{is a matrix of empty droplets from a CITEseq experiment using cell hashing antibodies called by Seurat's HTODemux function as negative cells, called by Demuxlet via referencing patient genotype data as ambiguous drops, and with less than 80 unique genes detected. This is used for robust estimation of the background distribution of each protein.}
 #' }
 "empty_drop_citeseq_mtx"
