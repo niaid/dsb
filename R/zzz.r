2902317d275a04ea655059ac3050b556b79c90c7
MattPM <mattmule@gmail.com>
2021-01-23 14:35:42 -0500

    cat lines on start message

--- R/zzz.r
+++ R/zzz.r
@@ -1,10 +1,9 @@
 dsbmessage <- function()
 {
   mesg <-
-    c(
-    "dsb package for CITE-seq protein normalization loaded ",
-    "please cite our paper at: https://www.biorxiv.org/content/10.1101/2020.02.24.963603v1 ",
-    "see vignette at https://github.com/niaid/dsb"
+    cat("dsb package for CITE-seq protein normalization loaded \n",
+        "please cite our paper at: https://www.biorxiv.org/content/10.1101/2020.02.24.963603v1 \n",
+        "see vignette at https://github.com/niaid/dsb"
     )
   return(mesg)
 }
