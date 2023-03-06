e751d9461709c225f039af68023998735a9dba7e
MattPM <mattmule@gmail.com>
2023-03-05 20:34:18 -0500

    update startup message for CRAN check on linux build

--- R/zzz.r
+++ R/zzz.r
@@ -1,17 +1,21 @@
-dsbmessage <- function()
-{
-  mesg <-
-    cat("loaded dsb package version", as.character(utils::packageVersion("dsb")),"please cite https://www.nature.com/articles/s41467-022-29356-8")
-  return(mesg)
-}
+# dsbmessage <- function()
+# {
+#   mesg <-
+#     cat("loaded dsb package version",
+#         as.character(utils::packageVersion("dsb")),
+#         "please cite https://www.nature.com/articles/s41467-022-29356-8"
+#     )
+#   return(mesg)
+# }
 
 .onAttach <- function(lib, pkg)
 {
   # startup message
-  msg <- dsbmessage()
-  if(!interactive())
-  msg[1] <- paste("Package 'dsb'")
-  packageStartupMessage(msg)
+  #msg <- dsbmessage()
+
+  packageStartupMessage(cat("loaded dsb package version",
+                            as.character(utils::packageVersion("dsb")),
+                            "please cite https://www.nature.com/articles/s41467-022-29356-8"
+  ))
   invisible()
 }
-
